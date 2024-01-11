907d4da988ca3462809f8572fc0345354484c302
skpaik <sdiptapaik@gmail.com>
2024-01-11 22:23:58 +0600

    # Feature - Home Page Design Done

--- src/app/_components/HeaderBanner.jsx
+++ src/app/_components/HeaderBanner.jsx
@@ -1,13 +1,13 @@
-export default function HeroBanner({ children, subtitle, title }) {
+export default function HeroBanner({children, title = "", subtitle = ""}) {
     return (
         <section className="bg-white text-center py-8 lg:py-12">
-                <div className="flex flex-col space-y-4 space-y-reverse">
-                    <h1 className="order-last text-lg text-gray-700">{subtitle}</h1>
+            <div className="flex flex-col space-y-4 space-y-reverse">
+                {title && <h1 className="text-5xl font-bold text-gray-900 sm:text-6xl">{title}</h1>}
 
-                    <h2 className="text-5xl font-bold text-gray-900 sm:text-6xl">{title}</h2>
-                </div>
+                {subtitle && <h2 className="order-last text-lg text-gray-700">{subtitle}</h2>}
+            </div>
 
-                <p className="mx-auto mt-6 max-w-lg text-base/relaxed text-gray-600">{children}</p>
+            {children && <p className="mx-auto mt-6 max-w-lg text-base/relaxed text-gray-600">{children}</p>}
         </section>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,89 @@
 import HeroBanner from "@/app/_components/HeaderBanner";
 import PageContents from "@/app/_components/PageContents";
+import {loadJsonContents} from "@/libs/JsonFileService";
+import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+interface HomeContents {
+    devFullName: string
+    pageTitle: string
+    pageSubTitle: string
+    titleDetail: string
+    techStackTitle: string
+    techStacks: [
+        {
+            label: string,
+            tech: string
+        }
+    ]
+    studyInfo: string
+}
+
+export default async function Home() {
+    const pageContent: HomeContents = await loadJsonContents("home")
     return (
         <>
-            <HeroBanner title="HyperUI"
-                        subtitle="Free Open Source Tailwind CSS Components">
-                HyperUI is a collection of free Tailwind CSS components that can be used in your next
-                project. With a range of components, you can build your next marketing website, admin
-                dashboard, eCommerce store and much more.
+            <HeroBanner title={pageContent.pageTitle}>
+
             </HeroBanner>
-            <PageContents classNames="">
-                <ul className="space-y-8">
-                    Home Contents
-                    eCommerce
-                    Working on an eCommerce project? HyperUI has a growing range of eCommerce Tailwind CSS components,
-                    that will help you build your next eCommerce website in Shopify, BigCommerce, Magento and more.
-                    eCommerce
-                    Working on an eCommerce project? HyperUI has a growing range of eCommerce Tailwind CSS components,
-                    that will help you build your next eCommerce website in Shopify, BigCommerce, Magento and more.
+            <PageContents classNames="overflow-hidden bg-gray-50 sm:grid sm:grid-cols-2 sm:items-center">
+                <div className="p-8 md:p-12 lg:px-16 lg:py-24">
+                    <div className="mx-auto max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
+                        <h2 className="text-2xl font-bold text-gray-900 md:text-3xl">
+                            {pageContent.devFullName}
+                        </h2>
 
+                        <p className="hidden text-gray-500 md:mt-4 md:block">
+                            {pageContent.pageSubTitle}
+                        </p>
 
-                    eCommerce
-                    Working on an eCommerce project? HyperUI has a growing range of eCommerce Tailwind CSS components,
-                    that will help you build your next eCommerce website in Shopify, BigCommerce, Magento and more.
+                        <p className="hidden text-gray-500 md:mt-4 md:block">
+                            {pageContent.titleDetail}
+                        </p>
+                        <p className="hidden text-gray-500 md:mt-4 md:block">
+                            {pageContent.studyInfo}
+                        </p>
+                        <div className="mt-4 md:mt-8">
+                            <Link
+                                href={"/contact"}
+                                className="inline-block rounded bg-emerald-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-emerald-700 focus:outline-none focus:ring focus:ring-yellow-400"
+                            >
+                                Hire
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+                <Image
+                    width={100}
+                    height={100}
+                    alt="Violin"
+                    src="https://avatars.githubusercontent.com/u/620762?s=400&u=353021b6974bb9baf267dc10ed116016d77ca618"
+                    className="h-full w-full object-cover sm:h-[calc(100%_-_2rem)] sm:self-end sm:rounded-ss-[30px] md:h-[calc(100%_-_4rem)] md:rounded-ss-[60px]"
+                />
+            </PageContents>
 
-                    eCommerce
-                    Working on an eCommerce project? HyperUI has a growing range of eCommerce Tailwind CSS components,
-                    that will help you build your next eCommerce website in Shopify, BigCommerce, Magento and more.
-                </ul>
+            <PageContents classNames="mt-12">
+                <span className="relative flex justify-center">
+                   <div
+                       className="absolute inset-x-0 top-1/2 h-px -translate-y-1/2 bg-transparent bg-gradient-to-r from-transparent via-gray-500 to-transparent opacity-75">
+                   </div>
+                    <span
+                        className="relative z-10 bg-white px-6 text-2xl font-extrabold text-gray-900 sm:text-3xl">{pageContent.techStackTitle}</span>
+                </span>
+                <div className="flow-root rounded-lg border border-gray-100 py-3 shadow-sm mt-6">
+                    <dl className="-my-3 divide-y divide-gray-100 text-sm">
+                        {
+                            pageContent.techStacks.map((item, index) => {
+                                return <div
+                                    className="grid grid-cols-1 gap-1 p-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4"
+                                    key={index}>
+                                    <dt className="font-medium text-gray-900">{item.label}</dt>
+                                    <dd className="text-gray-700 sm:col-span-2">{item.tech}</dd>
+                                </div>
+                            })
+                        }
+                    </dl>
+                </div>
             </PageContents>
         </>
     )

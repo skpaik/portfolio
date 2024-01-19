c8e84c49278b3a7806a2972c77e0d5ec9fce4911
skpaik <sdiptapaik@gmail.com>
2024-01-20 01:42:48 +0600

    Blog Details Page updated

--- src/app/_components/blog/BlogDetail.tsx
+++ src/app/_components/blog/BlogDetail.tsx
@@ -8,44 +8,43 @@ type Props = {
 }
 
 export default function BlogDetail({blogContent, currentPage}: Props) {
-    return <div className="max-w">
-        <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 items-center">
-            <div       className="relative block overflow-hidden rounded-lg border border-gray-100 p-4 sm:p-6 lg:p-8"            >
-                <span                    className="absolute inset-x-0 bottom-0 h-2 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"></span>
-                <div className="sm:flex sm:justify-between sm:gap-4">
-                    <div>
-                        <h3 className="text-lg font-bold text-gray-900 sm:text-xl">
-                            {blogContent.title}
-                        </h3>
-                        <p className="mt-1 text-xs font-medium text-gray-600">By Sudipta K Paik</p>
+    return <article className="rounded-xl bg-white ring ring-indigo-50 sm:p-6 lg:p-8">
+        <div className="flex items-start sm:gap-8">
+            <div>
+                <div className="mt-4 sm:flex sm:items-center sm:gap-2">
+                    <div className="flex items-center gap-1 text-gray-500">
+                        <svg
+                            className="h-4 w-4"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+                            ></path>
+                        </svg>
+
+                        <p className="text-xs font-medium">48:32 minutes</p>
                     </div>
-                    <div className="hidden sm:block sm:shrink-0">
-                        <img
-                            alt="Paul Clapton"
-                            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
-                            className="h-16 w-16 rounded-lg object-cover shadow-sm"
-                        />
-                    </div>
+                    <span className="hidden sm:block" aria-hidden="true">&middot;</span>
+                    <p className="mt-2 text-xs font-medium text-gray-500 sm:mt-0">
+                        Featuring <a href="#" className="underline hover:text-gray-700">Barry</a>,
+                        <a href="#" className="underline hover:text-gray-700">Sandra</a> and
+                        <a href="#" className="underline hover:text-gray-700">August</a>
+                    </p>
                 </div>
                 <div className="mt-4">
                     {blogContent.contents.map((para: string, index) => {
-                        return <p key={index} className="max-w-[40ch] text-sm text-gray-500 py-2">
+                        return <p key={index} className="text-sm text-gray-500 py-2">
                             {para}
                         </p>
                     })}
                 </div>
-                <dl className="mt-6 flex gap-4 sm:gap-6">
-                    <div className="flex flex-col-reverse">
-                        <dt className="text-sm font-medium text-gray-600">Published</dt>
-                        <dd className="text-xs text-gray-500">{toDateFormat(blogContent.dateTime)}</dd>
-                    </div>
-
-                    {/*<div className="flex flex-col-reverse">*/}
-                    {/*    <dt className="text-sm font-medium text-gray-600">Reading time</dt>*/}
-                    {/*    <dd className="text-xs text-gray-500">3 minute</dd>*/}
-                    {/*</div>*/}
-                </dl>
             </div>
         </div>
-    </div>
+    </article>
 }
--- src/app/blog/[page]/[slug]/page.tsx
+++ src/app/blog/[page]/[slug]/page.tsx
@@ -5,7 +5,6 @@ import {BlogContent, BlogMenu} from "@/app/_models/BlogModels";
 import {loadJsonContents} from "@/libs/JsonFileService";
 import PageContents from "@/app/_components/PageContents";
 import SideBar from "@/app/_components/blog/SideBar";
-import BlogList from "@/app/_components/blog/BlogList";
 import BlogDetail from "@/app/_components/blog/BlogDetail";
 
 export async function generateStaticParams() {
@@ -32,8 +31,7 @@ export default async function BlogSlug({params}: { params: Props }) {
     console.log(blogContent)
     return (
         <>
-            <HeroBanner title={page} subtitle={page}>
-                Blogs Page Slug {page}
+            <HeroBanner title={blogContent.title} subtitle={blogContent.intro}>
             </HeroBanner>
             <PageContents classNames="text-center">
                 <div className="grid gap-2 lg:grid-cols-4">
@@ -44,69 +42,6 @@ export default async function BlogSlug({params}: { params: Props }) {
                         <BlogDetail blogContent={blogContent} currentPage="page1"></BlogDetail>
                     </div>
                 </div>
-            </PageContents>
-            <PageContents classNames="">
-                <article className="rounded-xl bg-white p-4 ring ring-indigo-50 sm:p-6 lg:p-8">
-                    <div className="flex items-start sm:gap-8">
-                        <div
-                            className="hidden sm:grid sm:h-20 sm:w-20 sm:shrink-0 sm:place-content-center sm:rounded-full sm:border-2 sm:border-indigo-500"
-                            aria-hidden="true"
-                        >
-                            <div className="flex items-center gap-1">
-                                <span className="h-8 w-0.5 rounded-full bg-indigo-500"></span>
-                                <span className="h-6 w-0.5 rounded-full bg-indigo-500"></span>
-                                <span className="h-4 w-0.5 rounded-full bg-indigo-500"></span>
-                                <span className="h-6 w-0.5 rounded-full bg-indigo-500"></span>
-                                <span className="h-8 w-0.5 rounded-full bg-indigo-500"></span>
-                            </div>
-                        </div>
-
-                        <div>
-                            <strong
-                                className="rounded border border-indigo-500 bg-indigo-500 px-3 py-1.5 text-[10px] font-medium text-white"
-                            >
-                                Episode #101
-                            </strong>
-
-                            <h3 className="mt-4 text-lg font-medium sm:text-xl">
-                                <a href="" className="hover:underline"> {blogContent.title} </a>
-                            </h3>
-
-                            <p className="mt-1 text-sm text-gray-700">
-                                {blogContent.intro}
-                            </p>
-
-                            <div className="mt-4 sm:flex sm:items-center sm:gap-2">
-                                <div className="flex items-center gap-1 text-gray-500">
-                                    <svg
-                                        className="h-4 w-4"
-                                        fill="none"
-                                        stroke="currentColor"
-                                        viewBox="0 0 24 24"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            strokeWidth="2"
-                                            d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
-                                        ></path>
-                                    </svg>
-
-                                    <p className="text-xs font-medium">48:32 minutes</p>
-                                </div>
-
-                                <span className="hidden sm:block" aria-hidden="true">&middot;</span>
-
-                                <p className="mt-2 text-xs font-medium text-gray-500 sm:mt-0">
-                                    Featuring <a href="#" className="underline hover:text-gray-700">Barry</a>,
-                                    <a href="#" className="underline hover:text-gray-700">Sandra</a> and
-                                    <a href="#" className="underline hover:text-gray-700">August</a>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </article>
             </PageContents>
         </>
     )
